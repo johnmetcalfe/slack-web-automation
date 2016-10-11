e6573f04a4ee1fe7f42f906700d949b3cce88d31
johnmetcalfe <john.metcalfe@skybettingandgaming.com>
2016-10-11 15:30:52 +0100

    change username test

--- features/support/pages/account_page.rb
+++ features/support/pages/account_page.rb
@@ -1,7 +1,25 @@
 class SlackAccountPage < GenericPage
 
   def visit
-    @driver.get "https://slack-web-automation.slack.com/account/settings"
+    @driver.get "https://slack-web-automation2.slack.com/account/settings"
+  end
+
+  def expand_username
+    sleep 1
+    @driver.find_element(css: '#change_username > a').click
+
+  end
+
+  def change_username(user)
+    @driver.find_element(class: "username_input").clear
+    @driver.find_element(class: "username_input").send_keys user
+    @driver.find_element(css: "#change_username > div > form > div > button").click
+  end
+
+  def reset_username
+    @driver.find_element(class: "username_input").clear
+    @driver.find_element(class: "username_input").send_keys "slacktestbob"
+    @driver.find_element(css: "#change_username > div > form > div > button").click
   end
 
 end
