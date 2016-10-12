e9d45bba77a8b6e4876718958704f7c65054015b
Craig Pearce <craig.pearce@skybettingandgaming.com>
2016-10-12 10:58:24 +0100

    finished remaining tests for login and edited login feature - removed 'when i click continue' as it wasnt necessary

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,44 +1,57 @@
+wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+
 Given(/^i am on the sign in page$/) do
   @system.loginSlack.visit
-  expect(@browser.find_element(id: "domain").displayed?).not_to eq nil
+  expect(@browser.find_element(id: 'domain').displayed?).not_to eq nil
 end
 
 When(/^i enter the team name$/) do
-  @system.loginSlack.send_team_name("slack-web-automation2")
-  expect(@browser.find_element(id: "email").displayed?).not_to eq nil
+  @system.loginSlack.send_team_name('slack-web-automation2')
+  expect(@browser.find_element(id: 'email').displayed?).not_to eq nil
 end
 
 When(/^i enter my valid email and password$/) do
   @system.loginSlack.send_login_credentials('slacktestgerrard@gmail.com','slackpass')
+  expect(@browser.find_element(id: 'email')).not_to eq nil
 end
 
 When(/^i click sign in$/) do
   @system.loginSlack.click_enter_button
-  expect(@browser.find_element(id: "direct_messages").displayed?).not_to eq nil
+  #binding.pry
+  if @browser.find_elements(class: 'alert_error').size() > 0
+    expect(@browser.find_element(class: 'alert_error').displayed?).to eq true
+  else
+    wait.until {
+      @browser.find_element(id: 'direct_messages').displayed?
+    }
+    expect(@browser.find_element(id: 'direct_messages').displayed?).not_to eq nil
+  end
 end
 
 Then(/^i should see the general slack channel$/) do
-  @browser.get("https://slack-web-automation2.slack.com/messages/general/")
-  sleep 3
-  expect(@browser.find_element(id: 'channel_title').attribute('innerHTML')).to eq "#general"
+  @browser.get('https://slack-web-automation2.slack.com/messages/general/')
+  wait.until {
+    @browser.find_element(id: 'channel_title').displayed?
+  }
+  expect(@browser.find_element(id: 'channel_title').attribute('innerHTML')).to eq '#general'
 end
 
 When(/^i enter my invalid email and password$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+ @system.loginSlack.send_login_credentials('slacktestgerrard@gmail.co.uk','slackfail')
+ @system.loginSlack.click_enter_button
+ expect(@system.loginSlack.url).to eq 'https://slack-web-automation2.slack.com/'
+ expect(@browser.find_element(class: 'alert_error').displayed?).to eq true
 end
 
 Then(/^i should see an appropriate error message$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+ expect(@browser.find_element(class: 'alert_error').displayed?).to eq true
 end
 
 When(/^i enter the incorrect but a valid team name$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @system.loginSlack.send_team_name('skybettingandgaming')
 end
 
 When(/^i enter an invalid team name$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When(/^i click 'continue'$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @system.loginSlack.send_team_name('thisisainvalidslackname3257425')
+  expect(@browser.find_element(class: 'alert_error').displayed?).to eq true
 end
