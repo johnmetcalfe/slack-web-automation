4009629151fe9d9413e99b4fd8bc34c861420a3b
johnmetcalfe <john.metcalfe@skybettingandgaming.com>
2016-10-14 14:49:09 +0100

    for alfie

--- features/channels.feature
+++ features/channels.feature
@@ -5,40 +5,43 @@ Feature: Channels
     When I search for a channel
     Then It should be displayed on the page
 
-    Scenario: Create new private channel
+  Scenario: Create new private channel
+    Given I am logged in and on the messaging page
+    When I create a private channel
+    Then It should appear in my list of channels
 
+  Scenario: Create new public channel
+    Given I am logged in and on the messaging page
+    When I create a public channel
+    Then It should appear in my list of channels
 
-    Scenario: Create new public channel
+  Scenario: Sort channels by creation date
+    Given I am logged in and on the channels window
+    When I sort the channels by creation date
+    Then They should be listed in date order
 
+  Scenario: View archived channels
+    Given I am logged in and on the channels window
+    When I open archived channels
+    Then I should see the archived channels
 
-    Scenario: Sort channels by creation date
+  Scenario: Change channel
+    Given I am logged in and on general channel
+    When I pick a different channel
+    Then The channel should open
 
+  Scenario: About channels
+    Given I am logged in and on the channels window
+    When I open about channels
+    Then I should be redirected back to the messaging page
+    And Be shown an alert box
 
-    Scenario: View archived channels
+  Scenario: Assert notifications
+    Given I am logged in and on the messaging page
+    When I receive a message
+    Then The channel name should change to white
 
-
-    Scenario: Change channel
-
-
-    Scenario: About channel
-
-
-    Scenario: Assert notifications
-
-
-    Scenario: Change purpose of channel
-
-
-    Scenario: Send invites to channel
-
-
-    Scenario: View pinned items of channel
-
-
-    Scenario: View channel members
-
-
-    Scenario: View shared files
-
-
-    Scenario: Notification preferences
+  Scenario: Change purpose of channel
+    Given I am logged in and on the messaging page
+    When I change the purpose of the channel
+    Then It should be updated at the top of the page
---
+++ features/step_definitions/channels_steps.rb
@@ -0,0 +1,84 @@
+Given(/^I am logged in and on the channels window$/) do
+  @system.login_page.visit
+  @system.login_page.send_login_credentials(TestData.users[0][:email], TestData.users[0][:password])
+end
+
+When(/^I search for a channel$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^It should be displayed on the page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am logged in and on the messaging page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I create a private channel$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^It should appear in my list of channels$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I create a public channel$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I sort the channels by creation date$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^They should be listed in date order$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I open archived channels$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see the archived channels$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I am logged in and on general channel$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I pick a different channel$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^The channel should open$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I open about channels$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be redirected back to the messaging page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^Be shown an alert box$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I receive a message$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^The channel name should change to white$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I change the purpose of the channel$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^It should be updated at the top of the page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
