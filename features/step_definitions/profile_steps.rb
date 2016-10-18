271a851b97a2a054231645dc0bb9da879ca49167
johnmetcalfe <john.metcalfe@skybettingandgaming.com>
2016-10-18 10:11:52 +0100

    Initial setup

---
+++ features/step_definitions/profile_steps.rb
@@ -0,0 +1,43 @@
+Given(/^I am on the profile page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I change the profile First Name$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^The First Name should be updated on the page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I change the profile Last Name$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^The Last Name should be updated on the page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I chaneg the profile What I DO$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^The What I Do should be updated on the page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I change the profile Phone Number$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^The Phone Number shold be update on the page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I Change the profile Skype$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^The Skype should be updated on the page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
